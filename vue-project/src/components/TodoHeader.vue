997ce64fd3b2524323fb08f8473d648651196a4f
Mirnes <mirnes.kovacevic@gmail.com>
2024-03-20 12:25:18 +0100

    Section 1

---
+++ vue-project/src/components/TodoHeader.vue
@@ -0,0 +1,53 @@
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <h1>TODO</h1>
+                <h2>LIST</h2>
+            </div>
+            <ul class="nav-routes">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    .branding {
+        display: flex;
+        align-items: center;
+        h1 {
+            font-size: x-large;
+            font-weight: bolder;
+            color: purple;
+        }
+        h2 {
+            font-size: x-large;
+            font-weight: 200;
+            color: #1d1d1d;
+        }
+    }
+}
+
+nav ul {
+    display: flex;
+    gap: 12px;
+
+    a {
+    list-style: none;
+    text-decoration: none;
+    color: #1d1d1d;
+    }
+}
+</style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { uid } from "uid";
+import { ref } from "vue";
+import TodoCreator from "../components/TodoCreator.vue";
+
+const todoList = ref([]);
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: false,
+    isEditing: null,
+  });
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Create Todo</h1>
+    <TodoCreator @create-todo="createTodo" />
   </main>
 </template>
+
+
+<style lang="scss" scoped>
+main {
+    display: flex;
+    flex-direction: column;
+    max-width: 500px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 40px 16px;
+}
+
+h1 {
+    margin-bottom: 16px;
+    text-align: center;
+    color: purple;
+    font-size: xx-large;
+    font-weight: 500;
+}
+</style>
